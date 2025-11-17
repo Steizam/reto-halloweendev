ad06173b0246d1744b1b94d94b23e1a408ee3264
Xavier Gomez <15251233+Steizam@users.noreply.github.com>
2025-11-17 14:45:25 +0100

    Solución al Reto 4

--- Reto4.ts
+++ Reto4.ts
@@ -1,4 +1,27 @@
 function searchPhone(phones: number[], target: number): number {
-  // Your code here...
+  let left = 0
+  let right = phones.length - 1
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2)
+    const value = phones[mid]
+
+    if (value === target) {
+      return mid
+    }
+
+    if (target < value) {
+      right = mid - 1
+    } else {
+      left = mid + 1
+    }
+  }
+
   return -1
 }
+
+const phones = [1001, 1002, 1005, 1008, 1010, 1015, 1020]
+
+console.log(searchPhone(phones, 1008)) // → 3
+console.log(searchPhone(phones, 1020)) // → 6
+console.log(searchPhone(phones, 9999)) // → -1
